7e3779131e2c2af3f5ec1f539dd46d04b93c2f29
San-Rog <118987989+San-Rog@users.noreply.github.com>
2025-05-07 23:01:03 -0300

    Update uploadDataFrameCsvXlsxHtmlTxt.py

--- uploadDataFrameCsvXlsxHtmlTxt.py
+++ uploadDataFrameCsvXlsxHtmlTxt.py
@@ -6,6 +6,7 @@ import datetime
 from datetime import date
 from datetime import timedelta
 from io import BytesIO
+from xlsxwriter import Workbook
 
 def countCurUseFul(dateTuple):
     dateIni = dateTuple[0]
